90ffad1e4f983fe57fcadc852d6bdbf206aefed1
Emily <emily@mongodb.com>
2017-01-19 12:51:56 +0100

    Add evergreen files

---
+++ .evergreen/run-tests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -o xtrace   # Write all commands first to stderr
+set -o errexit  # Exit the script with error if any of the commands fail
+
+# Supported/used environment variables:
+#       MONGODB_URI             Set the suggested connection MONGODB_URI (including credentials and topology info)
+#       RVM_RUBY                Define the Ruby version to test with, using its RVM identifier.
+#                               For example: "ruby-2.3" or "jruby-9.1"
+
+MONGODB_URI=${MONGODB_URI:-}
+
+export CI=evergreen
+
+
+source ~/.rvm/scripts/rvm
+rvm install $RVM_RUBY
+rvm use $RVM_RUBY
+gem install bundler
+bundle install
+bundle exec rake clean
+bundle exec rake spec
